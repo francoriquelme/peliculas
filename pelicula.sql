7b0939f3d11e326c7ae495c21e0b488197fd8e43
francoriquelme <francoestebanru@gmail.com>
2022-04-12 18:33:50 -0400

    first commit

---
+++ pelicula.sql
@@ -0,0 +1,16 @@
+-- Requerimientos
+-- 1. Crear una base de datos llamada películas. (1 Punto)
+\c francoriquelme
+DROP DATABASE peliculas;
+CREATE DATABASE peliculas;
+
+-- 2. Cargar ambos archivos a su tabla correspondiente. (1 Punto)
+-- 3. Obtener el ID de la película “Titanic”. (1 Punto)
+-- 4. Listar a todos los actores que aparecen en la película "Titanic". (1 Puntos)
+-- 5. Consultar en cuántas películas del top 100 participa Harrison Ford. (2 Puntos)
+-- 6. Indicar las películas estrenadas entre los años 1990 y 1999 ordenadas por título de
+-- manera ascendente. (1 punto)
+-- 7. Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser
+-- nombrado para la consulta como “longitud_titulo”. (1 punto)
+-- 8. Consultar cual es la longitud más grande entre todos los títulos de las películas.
+-- (2 punto)
