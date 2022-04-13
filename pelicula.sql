41eab4cd20a6825fc0346467aa6eac4a15ef7cfb
francoriquelme <francoestebanru@gmail.com>
2022-04-12 20:24:45 -0400

    Finished

--- pelicula.sql
+++ pelicula.sql
@@ -12,28 +12,34 @@ CREATE TABLE peliculas(
     Año_estreno INT, 
     Director VARCHAR(50), 
     PRIMARY KEY(id)
-    );
+);
 
 \copy peliculas FROM 'peliculas.csv' csv header;
 SELECT * FROM peliculas LIMIT 10;
 
 CREATE TABLE reparto(
-        peliculas_id INT,
-        actores VARCHAR(100),
-        FOREIGN KEY(pelicula_id) REFERENCES peliculas(id) --mala práctica poner foreign key a ID
-    );
+    pelicula_id INT,
+    actores VARCHAR(100),
+    FOREIGN KEY(pelicula_id) REFERENCES peliculas(id) --mala práctica poner foreign key a ID
+);
+
 \copy reparto FROM 'reparto.csv' csv; -- Sin header porque no hay titulos en archivo reparto
 SELECT * FROM reparto LIMIT 10;
 
 -- 3. Obtener el ID de la película “Titanic”. (1 Punto)
-SELECT id FROM peliculas WHERE pelicula = 'Titanic';
+SELECT id FROM peliculas WHERE Pelicula = 'Titanic';
+
 -- 4. Listar a todos los actores que aparecen en la película "Titanic". (1 Puntos)
-SELECT actores FROM reparto WHERE peliculas_id = 2;
+SELECT actores FROM reparto WHERE pelicula_id = 2;
+
 -- 5. Consultar en cuántas películas del top 100 participa Harrison Ford. (2 Puntos)
-SELECT  COUNT(*) FROM reparto WHERE actores = 'Harrison Ford'; --en cuantos id está Harrison?? 8
+SELECT  COUNT(*) FROM reparto WHERE actores= 'Harrison Ford'; --en cuantos id está Harrison?? 8
+
 -- 6. Indicar las películas estrenadas entre los años 1990 y 1999 ordenadas por título de manera ascendente. (1 punto)
 SELECT Pelicula FROM peliculas WHERE Año_estreno BETWEEN 1990 AND 1999 ORDER BY Pelicula ASC;
--- 7. Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser
--- nombrado para la consulta como “longitud_titulo”. (1 punto)
--- 8. Consultar cual es la longitud más grande entre todos los títulos de las películas.
--- (2 punto)
+
+-- 7. Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser nombrado para la consulta como “longitud_titulo”. (1 punto)
+SELECT Pelicula, LENGTH(Pelicula) AS longitud_titulo FROM peliculas LIMIT 10;
+
+-- 8. Consultar cual es la longitud más grande entre todos los títulos de las películas. (2 punto)
+SELECT Pelicula, LENGTH(Pelicula) AS longitud_titulo FROM peliculas WHERE LENGTH(Pelicula) = (SELECT MAX(LENGTH(Pelicula)) FROM peliculas);
